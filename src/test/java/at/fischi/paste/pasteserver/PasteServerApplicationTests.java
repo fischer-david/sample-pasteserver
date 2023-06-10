9d35d477ca40133d00ae3133a8762f9e2f1b31a8
FishiGames <fishigamesyt@gmail.com>
2023-06-11 01:02:09 +0200

    Initial commit (Add Backend; Add Frontend with TailwindCSS)

---
+++ src/test/java/at/fischi/paste/pasteserver/PasteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fischi.paste.pasteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
