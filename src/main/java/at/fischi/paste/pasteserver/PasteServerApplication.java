2f6f88b04357b1bf27aab7b18ed428426e618eed
David Fischer <90455357+fischer-david@users.noreply.github.com>
2023-06-11 01:02:09 +0200

    Initial commit (Add Backend; Add Frontend with TailwindCSS)

---
+++ src/main/java/at/fischi/paste/pasteserver/PasteServerApplication.java
@@ -0,0 +1,13 @@
+package at.fischi.paste.pasteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteServerApplication.class, args);
+	}
+
+}
